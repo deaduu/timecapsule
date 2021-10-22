6356959f307f8fe68900dd29396f45ede81d1bba
Deepak shinde <iamthedpk@gmail.com>
2021-10-22 20:18:55 +0530

    all commit at once

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/{message:token}', [MessageController::class, 'getdata']);
+Route::post('savemsg', [MessageController::class, 'savemsg']);
